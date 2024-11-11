91247331132172e33959cbfc2c3f37d259a9d341
JovanMihic99 <jovanmihic99@gmail.com>
2024-11-11 17:04:29 +0100

    feat: add persistent caching

--- src/cache.js
+++ src/cache.js
@@ -1,9 +1,42 @@
 import NodeCache from "node-cache";
+import fs from "fs";
+import path from "path";
 
-const cacheOptions = {
-  stdTTL: 0, // 0 - indefinite TTL, (60 * 15) -  15 minutes
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const cacheFilePath = path.join(__dirname, "cache.json");
+
+const cache = new NodeCache();
+
+const loadCacheFromFile = () => {
+  try {
+    const data = fs.readFileSync(cacheFilePath, "utf-8");
+    const cacheData = JSON.parse(data);
+    cache.mset(cacheData);
+    console.log(`Loaded ${cacheData.length} cached entries from file.`);
+  } catch (error) {
+    console.warn("No cache file found. Starting with empty cache.");
+  }
 };
 
-const cache = new NodeCache(cacheOptions);
+const saveCacheToFile = () => {
+  const allKeys = cache.keys();
+
+  const entries = [];
+  for (const url in cache.data) {
+    if (Object.prototype.hasOwnProperty.call(cache.data, url)) {
+      let obj = { key: "", val: "" };
+      const element = cache.data[url];
+      obj.key = url;
+      obj.val = element.v;
+      entries.push(obj);
+    }
+  }
+  fs.writeFileSync(cacheFilePath, JSON.stringify(entries), "utf-8");
+  console.log("Cache saved to file");
+};
+
+loadCacheFromFile();
+
+setInterval(saveCacheToFile, 1000 * 30); // save cache every 30 seconds
 
 export default cache;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,4 @@
 import express from "express";
-import NodeCache from "node-cache";
 import axios from "axios";
 import cache from "../cache.js";
 
@@ -8,16 +7,17 @@ const router = express.Router();
 router.get("*", async (req, res) => {
   const newUrl = req.origin + req.url;
   const cachedResponse = cache.get(newUrl);
-
   if (cachedResponse) {
     res.setHeader("X-Cache", "HIT");
-    return res.status(200).json(cachedResponse.data);
+    return res.status(200).json(cachedResponse);
   }
   try {
-    res.setHeader("X-Cache", "MISS");
     const response = await axios.get(newUrl);
+    res.setHeader("X-Cache", "MISS");
+    res.set(response.headers);
     const data = response.data;
     cache.set(newUrl, data);
+    console.log(cache.data);
     return res.status(200).json(data);
   } catch (error) {}
 });
